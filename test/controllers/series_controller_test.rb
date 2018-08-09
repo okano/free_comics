226aa82a512bde50f581c1b8a46ddb5402867c5a
okano <okano@synergysoftware.jp>
2018-08-09 11:35:16 +0900

    titleタグを統一し、テストも修正。

--- test/controllers/series_controller_test.rb
+++ test/controllers/series_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class SeriesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = FreeComics::Application.config.site_title
+  end
+
   test "should get index" do
     get series_index_url
     assert_response :success
+    assert_select "title", "作品一覧 | #{@base_title}"
   end
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = FreeComics::Application.config.site_title
+    @site_title = FreeComics::Application.config.site_title
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | #{@site_title}"
   end
 
 end
