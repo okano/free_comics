754b3804c4003602591e6de1c3e5e45f30a643f1
okano <okano@synergysoftware.jp>
2018-08-09 10:57:23 +0900

    静的ページ(と画面ヘッダ/フッタなどの確認用)のcontroller等を追加。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
   root 'series#index'
   get 'series/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
